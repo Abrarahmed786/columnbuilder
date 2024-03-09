b26177839f7e535ef809d40c93c86e256dc430ac
Sourav Ranjan Maharana <saurav.maharana07@gmail.com>
2024-03-10 00:08:49 +0530

    Instead of blank inputs, now passing to corresponding field

--- lib/src/columnbuilder.dart
+++ lib/src/columnbuilder.dart
@@ -9,6 +9,7 @@ class ColumnBuilder extends StatelessWidget {
   final CrossAxisAlignment crossAxisAlignment;
   final TextDirection? textDirection;
   final VerticalDirection verticalDirection;
+  final bool growable;
 
   const ColumnBuilder({
     Key? key,
@@ -20,20 +21,28 @@ class ColumnBuilder extends StatelessWidget {
     this.crossAxisAlignment = CrossAxisAlignment.start,
     this.textDirection,
     this.verticalDirection = VerticalDirection.down,
+    this.growable,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
+      mainAxisSize: mainAxisSize,
+      verticalDirection: verticalDirection,
+      textDirection: textDirection,
       children: reverse == false
           ? List.generate(
               itemCount,
               (index) => itemBuilder(context, index),
+              growable: growable,
             ).toList()
           : List.generate(
               itemCount,
               (index) => itemBuilder(context, index),
             ).reversed.toList(),
+      growable: growable,
     );
   }
 }
